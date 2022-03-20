5d22ad1a628ad2b18c4badb4b0c8bbd3b6282a16
vvvvvvvvvvv894y89y894789yhfikudhioujio <100143643+vvvvvvvvvvv894y89y894789yhfikudhioujio@users.noreply.github.com>
2022-03-20 13:46:21 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ exports.news_API = '102074adc9b845c5a7eea4b2c857a19a';
 
 exports.giphy_API = 'TYqsXkOgzsyfdkGj7l6CrJQWtZk982mb';
 
-exports.AME_API = '';
+exports.AME_API = 'bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
